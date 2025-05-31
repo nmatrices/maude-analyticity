90bf3ce156a7df0741764b7b2d1b8be3f11d450a
Carlos Olarte <carlos.olarte@gmail.com>
2025-05-31 12:52:05 +0200

    Other cases and commands args for parallel.py

--- parallel.py
+++ parallel.py
@@ -1,31 +1,36 @@
 # An script to explore some branches of the search tree in parallel
-# for some logics that may create an important number of cases (as KD). 
+# for some logics that may create an important number of cases (as KD, K4). 
 
 import maude
 import multiprocessing
 import importlib
+import argparse
 
 
 def check(t, num):
     #maude = importlib.import_module('maude')
     #maude.init()
     #maude.load('KD')
-    KD = maude.getModule('CHECK-ANALITICITY-BOX')
-    strat = KD.parseStrategy("depC ; depA ; guess ! ; value' ! ; guess ! ; one(simplify) ! ; check")
+    LOGIC = maude.getModule('CHECK-ANALITICITY-BOX')
+    strat = LOGIC.parseStrategy("depC ; depA ; guess ! ; value' ! ; guess ! ; ignore ; (match no-case ? fail : idle) ; one(simplify) ! ; check") 
     for x,_ in t.srewrite(strat,depth=True):
         print(f'{num} : {x}')
 
     print(f'DONE({num})')
 
+def main():
+    parser = argparse.ArgumentParser(description="Checking in parallel the correctness of extensions")
+    parser.add_argument("--module", help="Maude file with the description of the logic (e.g., K4)")
+    args = parser.parse_args()
 
-if __name__ == '__main__':
-    #multiprocessing.set_start_method('spawn')
+
     maude.init()
-    maude.load('KD')
-    KD = maude.getModule('CHECK-ANALITICITY-BOX')
+
+    maude.load(f'{args.module}')
+    LOGIC = maude.getModule('CHECK-ANALITICITY-BOX')
     procs = []
-    strat = KD.parseStrategy('guess !')
-    t = KD.parseTerm('initBox')
+    strat = LOGIC.parseStrategy('guess !')
+    t = LOGIC.parseTerm('initBox')
 
     for x,i in t.srewrite(strat, depth=True):
         procs.append(multiprocessing.Process(target=check, args=(x, i )))
@@ -36,3 +41,6 @@ if __name__ == '__main__':
     for p in procs:
         p.join()
 
+
+if __name__ == '__main__':
+    main()
